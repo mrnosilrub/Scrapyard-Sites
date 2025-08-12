3e2d4ae3344f7531414752158acac3c844a593fe
mrnosilrub <eburlison@me.com>
2025-08-12 17:25:34 -0500

    commit and push

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  build: { inlineStylesheets: "auto" }
+  build: { inlineStylesheets: "auto", assetsPrefix: "./" }
   // If you know your canonical URL, set: site: "https://YOURDOMAIN"
   // For project pages like user.github.io/repo, you can add: base: "/REPO" (optional; avoid unless needed)
 });
