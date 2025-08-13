90b15f9922be439691741c32497f64589f361aa6
mrnosilrub <eburlison@me.com>
2025-08-13 17:57:29 -0500

    commit push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,34 @@
+import forms from "@tailwindcss/forms"
+import typography from "@tailwindcss/typography"
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
-	theme: {
-		extend: {
-			colors: {
-				brand: {
-					orange: "#D75E02",
-				},
-			},
-			boxShadow: {
-				header: "0 1px 2px rgba(0,0,0,0.06), 0 2px 8px rgba(0,0,0,0.04)",
-			},
-		},
-	},
-	plugins: [],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: [
+                    "ui-sans-serif",
+                    "system-ui",
+                    "-apple-system",
+                    "Segoe UI",
+                    "Roboto",
+                    "Noto Sans",
+                    "Ubuntu",
+                    "Cantarell",
+                    "Helvetica Neue",
+                    "Arial",
+                    "sans-serif",
+                ],
+            },
+            colors: {
+                brand: {
+                    orange: "#D75E02",
+                },
+            },
+            boxShadow: {
+                header: "0 1px 2px rgba(0,0,0,0.06), 0 2px 8px rgba(0,0,0,0.04)",
+            },
+        },
+    },
+    plugins: [forms, typography],
 }
