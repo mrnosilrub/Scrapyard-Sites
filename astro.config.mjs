027af43bf7f42a9e9a9ed9e30ce34da1a4f65bc0
mrnosilrub <eburlison@me.com>
2025-08-13 20:15:32 -0500

    chore: format, add Umami goals

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,10 @@ import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 
 export default defineConfig({
-    site: "https://scrapyardsites.com",
-    // base is only needed for GitHub Pages; remove for production domain
-    base: undefined,
+	site: "https://scrapyardsites.com",
+	// base is only needed for GitHub Pages; remove for production domain
+	base: undefined,
 	trailingSlash: "always",
-    integrations: [tailwind(), mdx(), sitemap()],
+	integrations: [tailwind(), mdx(), sitemap()],
 	build: { inlineStylesheets: "auto" },
 })
