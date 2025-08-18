476e15a953d511e38f7875c5366e1518cdcb1bf0
mrnosilrub <eburlison@me.com>
2025-08-18 03:50:56 -0500

    Update project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ import typography from "@tailwindcss/typography"
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+	safelist: [
+		"mobile-menu-overlay",
+		"menu-open",
+		"menu-showing",
+		"menu-content",
+		"mobile-nav-item",
+		"hamburger-icon",
+		"hamburger-line",
+	],
 	theme: {
 		extend: {
 			fontFamily: {
